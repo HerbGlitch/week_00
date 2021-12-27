575a77de0fe92d63a17e038908a0f11f5ad2244f
herbglitch <herbglitch@github.com>
2021-12-27 16:16:32 -0700

    possible breakage here

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 #include <ge/ge>
-#include "game/game.hpp"
+// #include "game/game.hpp"
 #include "splash/splash.hpp"
 
-GE_MAIN(tbyte::Game, 1920, 1080, "Prototype Week 00")
+GE_MAIN(tbyte::Splash, 1920, 1080, "Prototype Week 00")
--- src/splash/splash.cpp
+++ src/splash/splash.cpp
@@ -2,13 +2,14 @@
 
 namespace tbyte{
     Splash::Splash(){
-        ge::data->config.load("./res/config/defaults.config");
+        ge::data->config.load("./res/config/splash.config");
         ge::data->config.setGroup("");
 
-        SDL_Texture *tbyteSheet;
-        ge::data->config.get(GE_VAR_STR(tbyteSheet));
+        GE_Sprite *tbyteLogo;
+        ge::data->config.get(GE_VAR_STR(tbyteLogo));
 
-        logo = new ge::Sprite(ge::data->config.createSpriteParams(tbyteSheet, ge::SpriteStrs { .bounds = "tbyteBounds", .scale = "scale" }));
+        logo = new ge::Sprite(tbyteLogo);
+        // logo = new ge::Sprite(ge::data->config.createSpriteParams(tbyteSheet, ge::SpriteStrs { .bounds = "tbyteBounds", .scale = "scale" }));
         // logo = new ge::Sprite(tbyteSheet, tbyteBounds, ge::data->windowSize.w / 2 - (tbyteBounds.w * scale / 2), ge::data->windowSize.h / 2 - (tbyteBounds.h * scale / 2));
         // logo->setScale(scale);
 
