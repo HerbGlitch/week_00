f440143222f1cf7e3722d7b3237d8ace84ad3ee2
Langi Bryant <langibryant@gmail.com>
2021-12-28 00:50:45 -0700

    another fix for windows :D

---
+++ src/game/rts/mob/group/group.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <ge/ge>
+#include <vector>
+#include <queue>
+#include "../mob.hpp"
+#include "../soldier.hpp"
+
+namespace tbyte {
+    namespace rts {
+        namespace mob {
+            class Group {
+            public:
+                Group();
+
+                void update();
+
+                void addMob(Mob *mob);
+
+                void addCoord(SDL_Point pos);
+
+                bool empty();
+
+                uint32_t id;
+
+            private:
+                std::queue<SDL_Point> coords;
+                std::vector<Mob *> mobs;
+                SDL_Point current;
+                bool moving;
+            };
+        }
+    }
+}
